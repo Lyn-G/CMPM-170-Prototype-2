ced1c78af1f7cff45151e91eb461d637e2b94eb9
Lyn-G <132232498+Lyn-G@users.noreply.github.com>
2023-11-10 17:33:38 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+### Monster Chomp (WIP)
+
+This repository includes my prototype I created using the [crisp-game-lib](https://github.com/abagames/crisp-game-lib) by [abagames](https://github.com/abagames).
+
+I created this prototype as part of CMPM 170 - Rapid Prototyping's prototype assignment 2. It is a simple one-button input prototype where players need to correctly chomp on blue pieces and avoid chomping on red pieces.
+
+#### Things I Intend to Add
+
+- Adding teeth to the chomper.
+- Replacing the red and blue blocks with food pixel art.
+- Increase the speed of the block spawning as the player keeps playing.
+- Randomize the block spawning so it spawns at random intervals than a constant interval.
+- Update high score with the highest score the player managed to achieve before dying.
+
+Please click [here](https://lyn-g.github.io/Prototype-2-One-Button/) to play the prototype. Thank you so much for checking out my profile!
---
+++ main.js
@@ -0,0 +1,120 @@
+title = "Crush-tastic";
+description = `
+  Munch 
+  of blues,
+
+  Don't munch 
+  on reds.`;
+characters = [];
+options = {};
+
+let chomp1;
+let chomp2;
+let chomp1Y;
+let chomp2Y;
+const chompTime = 20;
+let chomp;
+let coin;
+let createCoin = false;
+let randColor;
+let coinColor;
+
+function update() {
+  if (!ticks) {
+    chomp = 1;
+    chomp1Y = -40;
+    chomp2Y = 60;
+    coin = [];
+    randColor = 0;
+    coinColor = 0;
+    score = 0;
+  }
+
+  // chompers
+  chomp1 = rect(30, chomp1Y, 40, 70);
+  chomp2 = rect(30, chomp2Y, 40, 70);
+
+  // only creates the coin if there isn't a coin on-screen
+  if (!createCoin) {
+    coin.push({
+      pos: vec(-5, 45),
+      vel: vec(difficulty, 0),
+      size: 5,
+    });
+    createCoin = true;
+  }
+
+  // randomly choose two colors for the coin
+  coinColor = randColor < 50 ? "blue" : "red";
+  color(coinColor);
+  console.log(coinColor);
+  console.log(randColor);
+
+  // when the function return true, it removes the coin
+  remove(coin, (b) => {
+    // this ensure that the coins move across the screen
+    b.pos.add(b.vel);
+
+    // check if the boxes are colliding
+    if (
+      box(b.pos, b.size).isColliding.rect.green ||
+      box(b.pos, b.size).isColliding.rect.light_green
+    ) {
+      // crush blue, do not crush red
+      if (coinColor == "red") {
+        createCoin = false;
+        end();
+      } else {
+        colorAndScore(1);
+      }
+
+      return true;
+    }
+
+    // leaving blue coins behind subtracts points
+    if (offscreen(b) && coinColor == "blue") {
+      colorAndScore(-1);
+    } else if (offscreen(b) && coinColor == "red") {
+      colorAndScore(0);
+    }
+
+    return offscreen(b);
+  });
+
+  // check if the user has pressed anything
+  if (input.isJustPressed) {
+    chomp = -1;
+  }
+
+  if (score < 0) end();
+
+  // these move the chompers up and down to touch the middle piece
+  if (chomp < 0) {
+    color("light_green");
+    chomp1Y += 4;
+    chomp2Y -= 4;
+
+    if (chomp1Y > -20 || chomp2Y < 40) {
+      chomp1Y -= chompTime;
+      chomp2Y += chompTime;
+
+      chomp = 1;
+    }
+  } else {
+    color("green");
+  }
+}
+
+function colorAndScore(score) {
+  createCoin = false;
+  colorChange();
+  addScore(score);
+}
+
+function colorChange() {
+  randColor = rnd(0, 100);
+}
+
+function offscreen(box) {
+  return box.pos.x > 110;
+}
